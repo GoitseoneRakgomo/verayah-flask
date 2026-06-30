906f87e0469d7142d05a5614da8fba305f43cf00
Goitseone Rakgomo <119797578+GoitseoneRakgomo@users.noreply.github.com>
2026-06-30 19:54:15 +0200

    init commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Home Page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Products Page
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
